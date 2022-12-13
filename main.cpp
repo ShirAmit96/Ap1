dce5d3325ff791f8fed29453697209488f228a1c
ShirAmit96 <92683819+ShirAmit96@users.noreply.github.com>
2022-12-13 22:40:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,22 +17,25 @@ int main(int argc, char *argv[])
     ReaderClass read=ReaderClass(fileName);
     DataBase db=read.readCsv();
     int columnsSize=db.db[0].size;
-    //get vector from the user and check if it is valid:
-    vector <double> secondInput= getSecondInput();
+	//create a knn class :
+	 Knn k_model = Knn(distanceMet, k, db.db);
     //check if k is not too big:
     if(k>db.db.size()){
         cout<<"Error: k value is bigger than data's size, exiting program..."<<endl;
         exit(-1);
     }
-    //Check if the inserted vector corresponds in size to the number of columns:
-    if(columnsSize==secondInput.size()){
-        //create a knn class and use it to predict the label:
-        Knn k_model = Knn(distanceMet, k, db.db);
-        string label =k_model.predict(secondInput);
-        cout << label <<endl;
-    }else{
-        cout<<"Error: the inserted vector is not valid,Exiting program..."<<endl;
-        exit(1);
-    }
+	while(true){
+		//get vector from the user and check if it is valid:
+		vector <double> secondInput= getSecondInput();
+		//Check if the inserted vector corresponds in size to the number of columns:
+		if(columnsSize==secondInput.size()){
+			//predict the label using knn class:  
+			string label =k_model.predict(secondInput);
+			cout << label <<endl;
+		}else{
+			cout<<"Error: the inserted vector is not valid,Exiting program..."<<endl;
+			exit(-1);
+		}
+	}
     return 1;
 }
