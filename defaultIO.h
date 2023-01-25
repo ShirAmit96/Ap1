d601d467ef796d0a5f673fc993c3c1ec04a909e4
GuyLeib <guyleib9@gmail.com>
2023-01-25 16:44:29 +0200

    testing and fixing

--- defaultIO.h
+++ defaultIO.h
@@ -1,16 +1,16 @@
-#ifndef AP1_DEFAULTIO_H
-#define AP1_DEFAULTIO_H
-#include <string>
-#include <iostream>
-#include <fstream>
-using namespace std;
-/*This class is an abstract class that has virtual read and write functions*/
-class DefaultIO{
-public:
-    virtual string read()=0;
-    virtual void write(string text)=0;
-    virtual ~DefaultIO(){}
-};
-
-
-#endif //AP1_DEFAULTIO_H
+//#ifndef AP1_DEFAULTIO_H
+//#define AP1_DEFAULTIO_H
+//#include <string>
+//#include <iostream>
+//#include <fstream>
+//using namespace std;
+///*This class is an abstract class that has virtual read and write functions*/
+//class DefaultIO{
+//public:
+//    virtual string read()=0;
+//    virtual void write(string text)=0;
+//    virtual ~DefaultIO(){}
+//};
+//
+//
+//#endif //AP1_DEFAULTIO_H
